ea1063c77d96043d80d4fa50e8a37bf90c5d8d66
Matheus Pereira <matheus.p.soares242@gmail.com>
2025-04-24 17:02:35 -0300

    first commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>Sistema de Tarefas</title>
+</head>
+<body>
+    <h1 class="font-bold text-4xl">Slk não compensa</h1>
+</body>
+</html>
